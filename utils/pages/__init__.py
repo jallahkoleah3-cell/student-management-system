34763c51130466df983d5367420b971ba48393fb
jallahkoleah3-cell <jallahkoleah3@gmail.com>
2026-08-07 09:40:37 -0600

    Add pages/__init__.py and remove duplicate firestore.rules

---
+++ utils/pages/__init__.py
@@ -0,0 +1,10 @@
+# utils/pages/__init__.py
+# This file makes the pages directory a Python package
+
+from . import admin_announcements
+from . import admin_dashboard
+from . import admin_roles
+from . import admin_students
+from . import admin_teachers
+from . import student_announcements
+from . import teacher_announcements
